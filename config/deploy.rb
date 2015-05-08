27fb366741853a11917078acb203b2734061dc9c
David Sljukic <davidsljukic@gmail.com>
2015-05-08 22:19:45 +0200

    deploy test

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'git@github.com:sljuka/nodetest.git'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/apps/nodetest'
 
-set :app_command, 'index.js'
+set :app_command, 'index'
 
 # Default value for :scm is :git
 # set :scm, :git
