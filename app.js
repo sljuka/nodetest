855ca923b589c29c49f7e7ecac018718f9ba8bbd
David Sljukic <davidsljukic@gmail.com>
2015-05-21 23:06:47 +0200

    dep

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var fs = require('fs')
+var morgan = require('morgan')
+
 var app = express();
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}))
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
