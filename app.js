5d4994147b85515512f9f096682cb7d2d0cb36b1
David Sljukic <davidsljukic@gmail.com>
2015-05-22 10:16:24 +0200

    app

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 var express = require('express');
-var fs = require('fs')
-var morgan = require('morgan')
 
 var app = express();
 
-// create a write stream (in append mode)
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
-
-// setup the logger
-app.use(morgan('combined', {stream: accessLogStream}))
-
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('I AM BORN IN World!');
 });
 
 var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log('Example app listening at http://%s:%s', server.address().address, server.address().port);
 });
